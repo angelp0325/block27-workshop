4c03624430bde3819326e22588962ecc2b0bc25b
Angel Polanco <angelepolanco325@gmail.com>
2025-11-12 13:52:31 -0500

    added useGuestDetails

---
+++ src/hooks/useGuestDetails.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { getGuestById } from "../api";
+
+export function useGuestDetails(id) {
+  const [guest, setGuest] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!id) return;
+
+    async function fetchGuest() {
+      try {
+        setLoading(true);
+        const data = await getGuestById(id);
+        setGuest(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchGuest();
+  }, [id]);
+
+  return { guest, loading, error };
+}
